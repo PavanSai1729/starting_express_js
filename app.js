e181ea734f5cd6b23f574c6172b57109aa25252f
PavanSai1729 <pavanmaths1729@gmail.com>
2024-05-27 20:27:34 +0530

    second Commit

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log("in the middleware");
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+app.use("/add-product", (req,res,next)=>{
+    res.send('<form action="/product" method="POST">Product : <input type="text" name="title"> Product Size: <input type="number" name="size"><button type="submit">Add Product</button></form>');
 });
 
-app.use((req,res,next)=>{
-    console.log("in another middleware");
-    res.send("<h1>hello form express</h1>");
+app.post("/product", (req, res, next) => {
+    console.log(`Product : ${req.body.title} and Product Size : ${req.body.size}`);
+    res.redirect('/');
 });
+
+app.get("/", (req,res,next)=>{
+    res.send("<h1>Product added</h1>");
+});
+
+
 
 app.listen(3000);
